0cff0064a6558b9d2c4174ab71a3c110d0697f84
Aaron Mercado <aaron.dev2898@gmail.com>
2025-07-23 19:02:46 +0800

    Rebuild portfolio

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -79,7 +79,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4ea2abbc1a69c047418f251d2364219f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "1659a5c2c2f374300b1b7b6da7a23410",
+"flutter_bootstrap.js": "b221cf3692f118f631bd68a67ba47861",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
